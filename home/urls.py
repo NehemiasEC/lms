98f741d72a3250003823b3ae72132c3b69782525
NehemiasEC <me@nehemiasec.me>
2025-10-08 22:14:12 -0400

    cskg

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomePageView
-
+from views import index
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('index', index, name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home/home.html"
+
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
